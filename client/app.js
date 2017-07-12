e6dab91906b97e5e9d19ab3656afa7cfcfa0309c
clarkwaite <clarkwaite@gmail.com>
2017-07-12 10:24:30 -0400

    fixing user login on heroku

--- client/app.js
+++ client/app.js
@@ -4,17 +4,19 @@ require("ng-token-auth");
 require("angular-cookie");
 
 angular.module("DiscoverSound", ["ui.router", "ipCookie", "ng-token-auth"])
-  .config(router, function($authProvider) {
-    $authProvider.configure({
-      apiUrl: '/',
-      validateOnPageLoad: false
-    });
-});
+  .config(router, auth);
 
+auth.$inject = ["$authProvider"];
+function auth($authProvider) {
+  $authProvider.configure({
+    apiUrl: '/',
+    validateOnPageLoad: false
+  });
+}
 router.$inject = ["$stateProvider", "$urlRouterProvider"];
-function router ($stateProvider, $urlRouterProvider) {
-	$stateProvider
-	.state("signIn", {
+function router($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("signIn", {
       url: "/sign_in",
       template: "<sign-in></sign-in>"
     })
@@ -22,62 +24,55 @@ function router ($stateProvider, $urlRouterProvider) {
       url: "/user_registration",
       template: "<user-registration></user-registration>"
     })
-		.state("home", {
-			url: "/",
-			template: "<discover-home></discover-home>",
-			resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
+    .state("home", {
+      url: "/",
+      template: "<discover-home></discover-home>",
+       resolve: {
+        auth: validateUser
       }
-		})
-		.state("artists", {
-			url: "/users/:userId/artists",
-			template: "<discover-artists></discover-artists>",
-			resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
+    })
+    .state("artists", {
+      url: "/users/:userId/artists",
+      template: "<discover-artists></discover-artists>",
+      resolve: {
+        auth: validateUser
       }
-		})
-		.state("artist", {
-			url: "/users/:userId/artists/:artistId",
-			template: "<discover-artist></discover-artist>",
-			params: {id: null, artist: null},
-			resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
+    })
+    .state("artist", {
+      url: "/users/:userId/artists/:artistId",
+      template: "<discover-artist></discover-artist>",
+      params: { id: null, artist: null },
+       resolve: {
+        auth: validateUser
       }
     })
     .state("related", {
-			url: "/users/:userId/artists/related",
-			template: "<discover-related></discover-related>",
-			params: {id: null, artist: null,},
-			resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
-      }
-		})
-		.state("events", {
-			url: "/users/:userId/events",
-			template: "<discover-events></discover-events>",
-			resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
-      }
-		})
-		.state("favorites", {
-      url: "/users/:userId/favorites",
-      template: "<discover-favorites></discover-favorites>",
+      url: "/users/:userId/artists/related",
+      template: "<discover-related></discover-related>",
+      params: { id: null, artist: null, },
       resolve: {
-        auth: function($auth) {
-          return $auth.validateUser();
-        }
+        auth: validateUser
       }
     })
-		
-	$urlRouterProvider.otherwise("/");
+    .state("events", {
+      url: "/users/:userId/events",
+      template: "<discover-events></discover-events>",
+      resolve: {
+        auth: validateUser
+      }
+    })
+    .state("favorites", {
+      url: "/users/:userId/favorites",
+      template: "<discover-favorites></discover-favorites>",
+       resolve: {
+        auth: validateUser
+      }
+    })
+
+  $urlRouterProvider.otherwise("/");
+}
+
+validateUser.$inject = ["$auth"]
+function validateUser($auth) {
+  return $auth.validateUser();
 }
