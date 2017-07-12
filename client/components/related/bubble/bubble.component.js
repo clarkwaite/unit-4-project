5c3698c3d36d483088339b44b62add587d678d9a
clarkwaite <clarkwaite@gmail.com>
2017-07-12 17:55:48 -0400

    need to push to heroku

--- client/components/related/bubble/bubble.component.js
+++
@@ -1,9 +0,0 @@
-import BubbleController from "./bubble.controller";
-import BubbleTemplate from "./bubble.html";
-
-  const bubbleComponent = {
-  	controller: BubbleController,
-  	template: BubbleTemplate
-  };
-
-  angular.module("DiscoverSound").component("discoverBubble", bubbleComponent);
