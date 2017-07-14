b42489f38a7ba8019c1a04c820207c018a10eb39
clarkwaite <clarkwaite@gmail.com>
2017-07-14 11:27:53 -0400

    fixed an issue with user login"

--- client/components/header/header.controller.js
+++ client/components/header/header.controller.js
@@ -1,23 +1,29 @@
 HeaderController.$inject = ['$auth', '$state'];
-function HeaderController($auth, $state){
+function HeaderController($auth, $state) {
   var vm = this;
   vm.currentUser = $auth.user;
   activate();
 
-  function activate(){
-  }
+  function activate() {
 
-    vm.signOut = function() {
-    $auth.signOut()
-      .then(function(response) {
-        console.log('successful sign out');
-        console.log(response.status);
-        $state.go('home');
-      })
-      .catch(function(response) {
-        console.log('error signing out');
-        console.log(response);
-      })
+    if (vm.currentUser) {
+      $state.go('home')
+    } else {
+      $state.go('signIn')
   }
+}
+
+vm.signOut = function () {
+  $auth.signOut()
+    .then(function (response) {
+      console.log('successful sign out');
+      console.log(response.status);
+      $state.go('signIn');
+    })
+    .catch(function (response) {
+      console.log('error signing out');
+      console.log(response);
+    })
+}
 }
 export default HeaderController;
